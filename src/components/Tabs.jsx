cef74f84d340febe29de1c2bba8cfa48515af104
thompson <alfredthompsonovie@gmail.com>
2023-09-01 19:55:06 +0100

    add active class to tabs btn

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -3,17 +3,17 @@ function Tabs({onActiveTab, activeTab}) {
 	return (
 		<section className="tabs__container">
       <button className="btn" onClick={()=>onActiveTab("all")}>
-				<span className="btn__text">All</span>
+				<span className={`btn__text ${activeTab === "all" ? "active" : ""}`}>All</span>
 				<span className={`line ${activeTab === "all" ? "active" : ""}`}></span>
 			</button>
 
 			<button className="btn" onClick={()=>onActiveTab("active")}>
-				<span className="btn__text">Active</span>
+				<span className={`btn__text ${activeTab === "active" ? "active" : ""}`}>Active</span>
         <span className={`line ${activeTab === "active" ? "active" : ""}`}></span>
 			</button>
 
 			<button className="btn" onClick={()=>onActiveTab("completed")}>
-				<span className="btn__text">completed</span>
+				<span className={`btn__text ${activeTab === "completed" ? "active" : ""}`}>completed</span>
 				<span className={`line ${activeTab === "completed" ? "active" : ""}`}></span>
 			</button>
 		</section>
